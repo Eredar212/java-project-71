57ba51f162f80c160596eb8238d993d45af75af5
Виктор Быков <viktor@Mac-mini-Viktor.local>
2023-10-01 15:56:52 +0300

    added more info about gendiff by '-h' calls

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,4 +7,10 @@ import picocli.CommandLine.Parameters;
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
 public class Differ {
+	@Parameters(index = "0", description = "path to first file.")
+	private String filepath1;
+	@Parameters(index = "1", description = "path to second file.")
+	private String filepath2;
+	@Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+	private String format = "stylish";
 }
