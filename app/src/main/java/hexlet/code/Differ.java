f03aa0376cdbe451ee05b53c35ee378d69ffa24c
Виктор Быков <viktor@Mac-mini-Viktor.local>
2023-10-01 16:03:07 +0300

    added paramLabel param

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -7,10 +7,10 @@ import picocli.CommandLine.Parameters;
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
 public class Differ {
-	@Parameters(index = "0", description = "path to first file.")
-	private String filepath1;
-	@Parameters(index = "1", description = "path to second file.")
-	private String filepath2;
-	@Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+	@Parameters(index = "0", paramLabel = "filepath1", description = "path to first file.")
+	private String filePath1;
+	@Parameters(index = "1", paramLabel = "filepath2", description = "path to second file.")
+	private String filePath2;
+	@Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
 	private String format = "stylish";
 }
