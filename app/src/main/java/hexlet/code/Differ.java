89d73ed5f8e240bad4b76b63ddfbbd2c37c4b5e7
Виктор Быков <viktor@Mac-mini-Viktor.local>
2023-10-07 19:11:32 +0300

    added generate with 2 params

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -13,4 +13,7 @@ public class Differ {
         Map<String, Map<String, Object>> diff = Difference.getDiff(map1, map2);
         return Formatter.getFormattedDiffString(diff, format);
     }
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        return generate(filePath1, filePath2, "stylish");
+    }
 }
