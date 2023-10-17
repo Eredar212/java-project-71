fdd81aeecb2817481299a99194f3b31b9c00ac83
Виктор Быков <viktor@Mac-mini-Viktor.local>
2023-10-17 18:36:57 +0300

    added getDataFormat private method

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,13 +1,12 @@
 package hexlet.code;
 
 import java.util.Map;
-import org.codehaus.plexus.util.FileUtils;
 
 public class Differ {
     public static String generate(String filePath1, String filePath2, String format) throws Exception {
         String content1 = Supplier.getData(filePath1);
         String content2 = Supplier.getData(filePath2);
-        Parser parser = ParserFactory.createParser(FileUtils.extension(filePath1).toUpperCase());
+        Parser parser = ParserFactory.createParser(getDataFormat(filePath1).toUpperCase());
         Map<String, Object> data1 = parser.parse(content1);
         Map<String, Object> data2 = parser.parse(content2);
         Map<String, Map<String, Object[]>> diff = DiffBuilder.getDiff(data1, data2);
@@ -16,5 +15,13 @@ public class Differ {
 
     public static String generate(String filePath1, String filePath2) throws Exception {
         return generate(filePath1, filePath2, "stylish");
+    }
+
+    // Формат данных берём на основе расширения файла, отрезая точку от строки.
+    private static String getDataFormat(String filePath) {
+        int index = filePath.lastIndexOf('.');
+        return index > 0
+                ? filePath.substring(index + 1)
+                : "";
     }
 }
